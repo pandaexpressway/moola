26d5241e80d6a3a5e905ff98d29218637f41d219
pandaexpressway <34220685+pandaexpressway@users.noreply.github.com>
2018-04-11 09:13:25 -0400

    Add files via upload

--- navigation.php
+++ navigation.php
@@ -18,15 +18,15 @@
       <a href="#" data-activates="mobile-demo" class="button-collapse"><i class="material-icons">menu</i></a>
       <ul class="right hide-on-med-and-down">
         <li><a href="index.php" class="white-text">Home</a></li>
-        <li><a href="#" class="white-text">About</a></li>
-        <li><a href="#" class="white-text">How it Works</a></li>
+        <li><a href="about.php" class="white-text">About</a></li>
+        <li><a href="how.php" class="white-text">How it Works</a></li>
         <li><a href="login.php" class="white-text">Login</a></li>
       </ul>
       <ul class="green accent-2" id="mobile-demo">
-        <li><a href="#" class="white-text">Home</a></li>
-        <li><a href="#" class="white-text">About</a></li>
-        <li><a href="#" class="white-text">How it Works</a></li>
-        <li><a href="#" class="white-text">Login</a></li>
+        <li><a href="index.php" class="white-text">Home</a></li>
+        <li><a href="about.php" class="white-text">About</a></li>
+        <li><a href="how.php" class="white-text">How it Works</a></li>
+        <li><a href="login.php" class="white-text">Login</a></li>
       </ul>
     </div>
    </nav>
