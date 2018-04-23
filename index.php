bb42f0c923c9988bdd54a5451ee52b0b19b8710f
cnitsch821 <38124147+cnitsch821@users.noreply.github.com>
2018-04-23 10:56:19 -0400

    Add files via upload

--- index.php
+++ index.php
@@ -19,19 +19,68 @@
 
 
     <!--STUFF-->
-        <div class="parallax-container">
-      <div class="parallax"><img src="img/cash.jpg"></div>
-    </div>
-    <div class="section white">
-      <div class="row container">
-        <h2 class="header">Parallax</h2>
-        <p class="grey-text text-darken-3 lighten-3">Just Parallax things.</p>
+<div class="container">
+  <div class="row">
+    <main class="col s8">
+      <h1>Basic page layout</h1>
+      <p class="flow-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duo Reges: constructio interrete. At certe gravius. Sic, et quidem diligentius saepiusque ista loquemur inter nos agemusque communiter. Sed erat aequius Triarium aliquid de dissensione nostra
+        iudicare. Traditur, inquit, ab Epicuro ratio neglegendi doloris.</p>
+      <div class="row">
+        <div class="col s12 m6">
+          <div class="card">
+            <div class="card-image">
+              <img src="https://picsum.photos/480/400">
+              <span class="card-title">Card Title</span>
+              <a class="btn-floating halfway-fab waves-effect waves-light red"><i class="material-icons">add</i></a>
+            </div>
+            <div class="card-content">
+              <p>I am a very simple card. I am good at containing small bits of information. I am convenient because I require little markup to use effectively.</p>
+            </div>
+          </div>
+        </div>
+        <div class="col s12 m6">
+          <div class="card">
+            <div class="card-image">
+              <img src="https://picsum.photos/480/400">
+              <span class="card-title">Card Title</span>
+              <a class="btn-floating halfway-fab waves-effect waves-light red"><i class="material-icons">add</i></a>
+            </div>
+            <div class="card-content">
+              <p>I am a very simple card. I am good at containing small bits of information. I am convenient because I require little markup to use effectively.</p>
+            </div>
+          </div>
+        </div>
       </div>
-    </div>
-    <div class="parallax-container">
-      <div class="parallax"><img src="img/mon.gif"></div>
-    </div>
-
+    </main>
+<!--SIDEBAR LOGIN-->
+    <aside class="sidebar col s4">
+      <div class="row">
+        <div class="col s12">
+          <div class="card-panel">
+            <div class="row">
+       <form class="s12 m4" method="POST" action="accountCreation.php">
+            <div class="row">
+        <h5><center> View Your Account</h5></center>
+         <div class="input-field">
+          <i class="material-icons prefix">account_circle</i>
+           <input type="text" name="user" id="name">
+           <label class="active" for="name">Username</label>
+         </div>
+         <div class="input-field">
+          <i class="material-icons prefix">lock</i>
+           <input type="password" name="pass" id="password" class="validate">
+           <label class="active" for="password">Password</label>
+         </div>
+         <center><button class="waves-effect waves-light btn teal accent-4" name="submit">Sign On</button><br></center>
+          <center><br><hr><p>New to moola?<a href="register.php">Register here!</a></p></center>
+       </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </aside>
+  </div>
+</div>
 
     	<!---FOOTER-->	
 		<?php
@@ -47,7 +96,8 @@
     </script>
     <script>
     $(document).ready(function(){
-    $('.parallax').parallax();
+    	$('.parallax').parallax();
+	$(".button-collapse").sideNav();
     });        
 		</script>
 
