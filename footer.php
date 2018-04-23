a7ffe1f91b4d4b7e58d6fce16cd61885f7e80e81
victoriaka <vkate11@gmail.com>
2018-04-23 09:22:03 -0400

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,54 +1,28 @@
-
-  <!DOCTYPE html>
-  <html>
-    <head>
-      <!--Import Google Icon Font-->
-      <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-      <!--Import materialize.css-->
-      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/css/materialize.min.css">
-      <!--Let browser know website is optimized for mobile-->
-      <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-      <title>Moola</title>
-    </head>
-
-
-	<body>
-
-            <footer class="indigo darken-3">
-          <div class="container">
-            <div class="row">
-              <div class="col l6 s12">
-                <h5 class="white-text">Contact Us</h5>
-                <ul class="white-text">
-                  <li><i class="material-icons">phone</i>&nbsp;&nbsp;(407) 906-3034</li> 
-                  <li><i class="material-icons ">location_on</i>&nbsp;&nbsp;225 E Robinson St #660, Orlando, FL 32801</li> 
-                  <li><i class="material-icons ">email</i>&nbsp;&nbsp;hello@moola.com</li>            
-              </ul>
-              </div>
-              <div class="col l4 offset-l2 s12">
-                <h5 class="white-text">Follow Us!</h5>
-                <ul>
-                  <li><a class="white-text" href="#">Facebook</a></li>
-                  <li><a class="white-text" href="#">Instagram</a></li>
-                  <li><a class="white-text" href="#">Twitter</a></li>
-                  <li><a class="white-text" href="#">LinkedIn</a></li>
-                </ul>
-              </div>
-            </div>
-          </div>
-          <div class="footer-copyright">
-            <div class="container">
-           <a class="white-text">&#169; 2018 MOOLA. ALL RIGHTS RESERVED</a>
-            </div>
-          </div>
-        </footer>
-
-      <!--Import jQuery before materialize.js-->
-      <script type="text/javascript" src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
-		<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/js/materialize.min.js"></script>
-		<script>
-		$(".button-collapse").sideNav();
-		</script>
-
-    </body>
-</html>
+<footer class="indigo darken-3">
+  <div class="container">
+    <div class="row">
+      <div class="col l6 s12">
+	<h5 class="white-text">Contact Us</h5>
+	<ul class="white-text">
+	  <li><i class="material-icons">phone</i>&nbsp;&nbsp;(407) 906-3034</li> 
+	  <li><i class="material-icons ">location_on</i>&nbsp;&nbsp;225 E Robinson St #660, Orlando, FL 32801</li> 
+	  <li><i class="material-icons ">email</i>&nbsp;&nbsp;hello@moola.com</li>            
+      </ul>
+      </div>
+      <div class="col l4 offset-l2 s12">
+	<h5 class="white-text">Follow Us!</h5>
+	<ul>
+	  <li><a class="white-text" href="#">Facebook</a></li>
+	  <li><a class="white-text" href="#">Instagram</a></li>
+	  <li><a class="white-text" href="#">Twitter</a></li>
+	  <li><a class="white-text" href="#">LinkedIn</a></li>
+	</ul>
+      </div>
+    </div>
+  </div>
+  <div class="footer-copyright">
+    <div class="container">
+   <a class="white-text">&#169; 2018 MOOLA. ALL RIGHTS RESERVED</a>
+    </div>
+  </div>
+</footer>
