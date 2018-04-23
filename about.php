0294bd2998fb928ea068de827097c617803b3002
victoriaka <vkate11@gmail.com>
2018-04-23 10:13:28 -0400

    Update about.php

--- about.php
+++ about.php
@@ -42,12 +42,11 @@
       <script type="text/javascript" src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/js/materialize.min.js"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
-		<script>
-    $(".button-collapse").sideNav();
-    </script>
+
     <script>
     $(document).ready(function(){
     	$('.parallax').parallax();
+	$(".button-collapse").sideNav();
     });        
 		</script>
 
