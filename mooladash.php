65bc4f78eb025d001dc0d0aeba13e9b4cf25d4b1
victoriaka <vkate11@gmail.com>
2018-04-23 09:55:06 -0400

    Update mooladash.php

--- mooladash.php
+++ mooladash.php
@@ -86,23 +86,10 @@ echo "['".$row['description']."',".$row['bill']."],"; ?>
 <!---<img src="trump.png" alt="Smiley face" height="42" width="42">
 -->
 
-<nav class="indigo darken-3">
-    <a href="index.php" class="brand-logo"><img src="img/moola_gradient_1.png" width="64px" height="64px"></a>
-    <a href="#" data-activates="mobile-demo" class="button-collapse"><i class="material-icons">menu</i></a>
-    <ul class="right hide-on-med-and-down">
-      <li><a href="index.php" class="white-text">Home</a></li>
-      <li><a href="about.php" class="white-text">About</a></li>
-      <li><a href="how.php" class="white-text">How it Works</a></li>
-      <li><a href="login.php" class="white-text">Login</a></li>
-    </ul>
-    <ul class="green accent-2" id="mobile-demo">
-      <li><a href="index.php" class="white-text">Home</a></li>
-      <li><a href="about.php" class="white-text">About</a></li>
-      <li><a href="how.php" class="white-text">How it Works</a></li>
-      <li><a href="login.php" class="white-text">Login</a></li>
-    </ul>
-  </div>
- </nav>
+<?php 
+    include_once("navigation.php");
+?>
+ 
   <div class="section no-pad-bot" id="index-banner">
     <div class="container">
       <br><br>
