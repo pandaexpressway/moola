696e46d5c59b231bdfec301d9051f44f9605780f
pandaexpressway <34220685+pandaexpressway@users.noreply.github.com>
2018-04-24 18:15:00 -0400

    fixed sidenav

--- about.php
+++ about.php
@@ -19,18 +19,7 @@
 
 
     <!--STUFF-->
-        <div class="parallax-container">
-      <div class="parallax"><img src="img/cash.jpg"></div>
-    </div>
-    <div class="section white">
-      <div class="row container">
-        <h2 class="header">Parallax</h2>
-        <p class="grey-text text-darken-3 lighten-3">Just Parallax things.</p>
-      </div>
-    </div>
-    <div class="parallax-container">
-      <div class="parallax"><img src="img/mon.gif"></div>
-    </div>
+
 
 
     	<!---FOOTER-->	
@@ -39,16 +28,13 @@
 		?>
 
       <!--Import jQuery before materialize.js-->
-      <script type="text/javascript" src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
-		<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/js/materialize.min.js"></script>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
-
+    <script type="text/javascript" src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/js/materialize.min.js"></script>
     <script>
     $(document).ready(function(){
-    	$('.parallax').parallax();
-	$(".button-collapse").sideNav();
-    });        
-		</script>
+    $(".button-collapse").sideNav();
+         });  
+    </script>
 
 
               </body>
