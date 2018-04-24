fc534a3e0a7904af8ead1568d16db6980b11038b
pandaexpressway <34220685+pandaexpressway@users.noreply.github.com>
2018-04-24 18:15:56 -0400

    fixed sidenav

--- index.php
+++ index.php
@@ -88,18 +88,14 @@
 		?>
 
       <!--Import jQuery before materialize.js-->
-      <script type="text/javascript" src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
-		<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/js/materialize.min.js"></script>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
-		<script>
-    $(".button-collapse").sideNav();
-    </script>
+    <script type="text/javascript" src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/js/materialize.min.js"></script>
     <script>
     $(document).ready(function(){
-    	$('.parallax').parallax();
-	$(".button-collapse").sideNav();
-    });        
-		</script>
+    $(".button-collapse").sideNav();
+         });  
+    </script>
+
 
 
               </body>
